85ab65aff4da1e0f8d56fb81386c3f4138803d0d
psutinboy <grantledbetter12@gmail.com>
2025-01-13 15:39:01 -0600

    Upgrade Bootstrap to version 5.3.3 and refactor carousel implementation in Home component to use Bootstrap 5 syntax and features.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,34 +1,41 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 
 const Home = () => {
+    useEffect(() => {
+        // Initialize Bootstrap carousel
+        if (typeof window !== 'undefined') {
+            require('bootstrap/dist/js/bootstrap.bundle.min.js');
+        }
+    }, []);
+
     return (
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="versatiledeltatrack.jpg" alt="Slide showcasing a versatile deltatrack tractor"/>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide"/>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide"/>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+            <ol className="carousel-indicators">
+                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"></li>
+                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img className="d-block w-100" src="versatiledeltatrack.jpg" alt="Slide showcasing a versatile deltatrack tractor"/>
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src="..." alt="Second slide"/>
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src="..." alt="Third slide"/>
+                </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </a>
+        </div>
     );
-    };
+};
+
 export default Home;    
