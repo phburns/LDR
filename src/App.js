a306f6646de0fc3a35af823e87418aada37d6265
phburns <phburns@gus.pittstate.edu>
2025-01-13 13:19:41 -0600

    added navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import NavbarHook from './components/NavbarHook/NavbarHook';
 
 function App() {
   return (
+  <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavbarHook/>
+        <Footer/>
       </header>
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/NavbarHook/NavbarHook.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+import {useMediaQuery} from "react-responsive";
+import "./NavbarHook.css";
+
+const NavbarHook = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: "1150px" });
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    if (isMobile) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  const renderNavLinks = () => {
+    const listClassName = isMobile ? "nav__list" : "nav__list__web";
+    const linkClassName = "nav__link";
+
+    return (
+      <ul className={listClassName}>
+        <li className="nav__item">
+          <NavLink to="/" className={linkClassName} onClick={closeMobileMenu}>
+            Home
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink to="/products" className={linkClassName} onClick={closeMobileMenu}>
+            Products
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink to="/userbuilds" className={linkClassName} onClick={closeMobileMenu}>
+            User Builds
+          </NavLink>
+        </li>
+        <li className="nav__item">
+          <NavLink to="/yourlist" className={linkClassName} onClick={closeMobileMenu}>
+            Your List
+          </NavLink>
+        </li>
+      </ul>
+    );
+  };
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <div className="navbar-brand">
+          <NavLink to="/" className="nav__logo">
+            Larry's Diesel and Repair
+          </NavLink>
+        </div>
+
+        {isMobile && (
+          <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+            <IoMenu />
+          </div>
+        )}
+
+        {isMobile ? (
+          <div
+            className={`nav__menu ${isMenuOpen ? "show-menu" : ""}`}
+            id="nav-menu"
+          >
+            <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+              <IoClose />
+            </div>
+            {renderNavLinks()}
+          </div>
+        ) : (
+          <div className="nav__menu">
+            {renderNavLinks()}
+          </div>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default NavbarHook;
