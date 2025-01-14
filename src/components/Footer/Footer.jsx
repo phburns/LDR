be78dc0bcbaeb4da35e4e50434d6195914227963
phburns <phburns@gus.pittstate.edu>
2025-01-14 12:27:06 -0600

    stuff and things to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,11 @@ const Footer = () => {
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} Larry's Diesel and Repair, LLC. All rights reserved. </p>
       <br/>
-      <p> Got questions or concerns? <a href='/contactus'>Send us an email</a> or give us a call at (620)-231-5420.</p>
+      <p> Got questions or concerns? Feel free to <a href='/contactus'>send us an email</a> or give us a call.</p>
+      <br/>
+      <p>While you're down here why not check out our socials too? </p>
+     
+
     </footer>
   );
 };
