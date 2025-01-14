d0ca7bf069098884fa57c041bea884c584ffe280
phburns <phburns@gus.pittstate.edu>
2025-01-14 13:22:04 -0600

    added facebook logo to footer and have it link out to the facebook page upon click

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import NavbarHook from './components/NavbarHook/NavbarHook';
-import Home from './pages/home';
-import Inventory from './pages/inventory';
-import Partners from './pages/partners';
-import ContactUs from './pages/contactus';
+import Home from './pages/home/home';
+import Inventory from './pages/inventory/inventory';
+import Partners from './pages/partners/partners';
+import ContactUs from './pages/contactus/contactus';
 
 function App() {
   return (
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './footer.css';
+import { Icon } from '@iconify/react';
+import facebookIcon from '@iconify-icons/mdi/facebook';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; {new Date().getFullYear()} Larry's Diesel and Repair, LLC. All rights reserved.</p>
-        <p>Got questions or concerns? <a href='/contactus'>Send us an email</a> or give us a call. Also check out our facebook to stay up to date with us!</p> <iconify-icon icon='baseline-facebook'></iconify-icon>
-        
+        <p>Got questions or concerns? <a href='/contactus'>Send us an email</a> or give us a call. Also check out our facebook to stay up to date with us!
+         <a href="https://www.facebook.com/larrysdieselrepair/" target="_blank" rel="noopener noreferrer">
+           <Icon icon={facebookIcon} className="facebook-icon" />
+         </a>
+        </p>
       </div>
     </footer>
   );
