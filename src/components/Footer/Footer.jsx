305739e7c05ac996fc2a2d6e34ca0f6db10f0c62
phburns <phburns@gus.pittstate.edu>
2025-01-15 13:49:48 -0600

    updated footer so that it only appears when scrolled to the bottom of the screen needs an update for the inventory page though

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './footer.css';
 import { Icon } from '@iconify/react';
 import facebookIcon from '@iconify-icons/mdi/facebook';
 
 const Footer = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const scrolledToBottom = scrollTop + windowHeight >= documentHeight - 10;
+
+      setIsVisible(scrolledToBottom);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Check initial load
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <footer className="footer">
+    <footer className={`footer ${isVisible ? 'visible' : ''}`}>
       <div className="footer-content">
         <p>&copy; {new Date().getFullYear()} Larry's Diesel and Repair, LLC. All rights reserved.</p>
         <p>Got questions or concerns? <a href='/contactus'>Send us an email</a> or give us a <a href='/contactus'>call</a>. Also check out our facebook to stay up to date with us!
