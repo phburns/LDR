f00a04dacbc58389389c0fe9beb894e584b68662
phburns <phburns@gus.pittstate.edu>
2025-01-16 14:48:50 -0600

    added favicon but no worky worky currently

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import AdminLogin from './components/AdminLogin/AdminLogin';
-import Footer from './components/Footer/Footer';
-import NavbarHook from './components/NavbarHook/NavbarHook';
-import NotFound from './components/NotFound/NotFound';
-import AdminPage from './pages/admin/admin';
-import ContactUs from './pages/contactus/contactus';
-import Home from './pages/home/home';
-import Inventory from './pages/inventory/inventory';
-import Repair from './pages/repair/repair';
-import Layout from './components/Layout/Layout';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import AdminLogin from "./components/AdminLogin/AdminLogin";
+import Footer from "./components/Footer/Footer";
+import NavbarHook from "./components/NavbarHook/NavbarHook";
+import NotFound from "./components/NotFound/NotFound";
+import AdminPage from "./pages/admin/admin";
+import ContactUs from "./pages/contactus/contactus";
+import Home from "./pages/home/home";
+import Inventory from "./pages/inventory/inventory";
+import Repair from "./pages/repair/repair";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
@@ -20,22 +20,27 @@ function App() {
         <header>
           <NavbarHook />
         </header>
-        <main className='main-content'>
-        <Layout>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/inventory' element={<Inventory />} />
-            <Route path='/inventory/:brand' element={<Inventory />} />
-            <Route path='/repair' element={<Repair />} />
-            <Route path='/contactus' element={<ContactUs />} />
-            <Route path='/admin' element={
-              localStorage.getItem('adminAuthenticated') === 'true' 
-                ? <AdminPage /> 
-                : <AdminLogin />
-            } />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </Layout>
+        <main className="main-content">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/inventory" element={<Inventory />} />
+              <Route path="/inventory/:brand" element={<Inventory />} />
+              <Route path="/repair" element={<Repair />} />
+              <Route path="/contactus" element={<ContactUs />} />
+              <Route
+                path="/admin"
+                element={
+                  localStorage.getItem("adminAuthenticated") === "true" ? (
+                    <AdminPage />
+                  ) : (
+                    <AdminLogin />
+                  )
+                }
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
         </main>
         <Footer />
       </div>
