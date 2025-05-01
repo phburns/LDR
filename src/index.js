34bc45ff5f8c1732bff820ac9e932c1584f5feae
Parker Burns <phburns@gus.pittstate.edu>
2025-05-01 16:56:26 -0500

    fixed index.css import into index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '/index.css';
 import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
